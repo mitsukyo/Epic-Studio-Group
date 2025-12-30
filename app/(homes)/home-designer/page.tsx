a83df190d49b9e7cb6146ed239d473d1e2887244
mitsukyo <ameachellenicole@gmail.com>
2025-12-30 16:00:05 +0800

    Updated Name of AuthoritySlider

--- app/(homes)/home-designer/page.tsx
+++ app/(homes)/home-designer/page.tsx
@@ -3,12 +3,12 @@ import Footer2 from "@/components/footers/Footer2";
 import About from "@/components/homes/home-designer/About";
 import Hero from "@/components/homes/home-designer/Hero";
 import MarqueeSlider from "@/components/homes/home-designer/MarqueeSlider";
-import Projects from "@/components/homes/home-designer/Projects";
 import Services from "@/components/homes/home-designer/Services";
 import { Metadata } from "next";
+import AuthoritySlider from "@/components/homes/home-designer/AuthoritySlider";
 export const metadata: Metadata = {
   title:
-    "Designer || Rayo - Digital Agency & Personal Portfolio React Nextjs Template",
+    "Epic Studio Group - We Turn CEO-Authors into Media Brands",
   description:
     "Rayo - Digital Agency & Personal Portfolio React Nextjs Template",
 };
@@ -17,7 +17,7 @@ export default function HomeDesignerPage() {
     <>
       <main id="mxd-page-content" className="mxd-page-content">
         <Hero />
-        <Projects />
+        <AuthoritySlider /> 
         <MarqueeSlider />
         <About />
         <Services />
