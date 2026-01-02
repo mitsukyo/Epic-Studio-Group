156a6c41e82451537307b41d1746918283564be0
Your Name <shanedelatorre2412@gmail.com>
2026-01-02 17:46:19 +0800

    Remove unnecessary files

--- app/(homes)/home/page.tsx
+++ app/(homes)/home/page.tsx
@@ -1,20 +1,20 @@
 import Footer2 from "@/components/footers/Footer2";
 
-import About from "@/components/homes/home-designer/About";
-import Hero from "@/components/homes/home-designer/Hero";
-import MarqueeSlider from "@/components/homes/home-designer/MarqueeSlider";
-import Services from "@/components/homes/home-designer/Services";
+import About from "@/components/homes/About";
+import Hero from "@/components/homes/Hero";
+import MarqueeSlider from "@/components/homes/MarqueeSlider";
+import Services from "@/components/homes/Services";
 import { Metadata } from "next";
-import AuthoritySlider from "@/components/homes/home-designer/AuthoritySlider";
+import AuthoritySlider from "@/components/homes/AuthoritySlider";
 import Cta from "@/components/common/Cta";
-import MarqueeSlider2 from "@/components/homes/home-web-agency/MarqueeSlider2";
+import MarqueeSlider2 from "@/components/homes/MarqueeSlider2";
 export const metadata: Metadata = {
   title:
     "Epic Studio Group - We Turn CEO-Authors into Media Brands",
   description:
-    "Rayo - Digital Agency & Personal Portfolio React Nextjs Template",
+    "Epic Studio Group - We Turn CEO-Authors into Media Brands",
 };
-export default function HomeDesignerPage() {
+export default function HomePage() {
   return (
     <>
       <main id="mxd-page-content" className="mxd-page-content">
@@ -25,9 +25,7 @@ export default function HomeDesignerPage() {
         <Services />
         <MarqueeSlider2 />
         <Cta /> 
-      
       </main>
-      
       <Footer2 text="epicstudiogroup" />
     </>
   );
